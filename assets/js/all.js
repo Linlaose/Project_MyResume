74dd1623489e9ce33b221c7868230539afc3197f
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-10-25 23:24:25 +0800

    Update 2022-10-25T15:24:20.515Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,20 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
-});
+}); // burger
+
+var burger = document.querySelector('.burger');
+var topLine = document.querySelector('.burger__topline');
+var midLine = document.querySelector('.burger__midline');
+var btmLine = document.querySelector('.burger__btmline');
+
+if (burger) {
+  burger.addEventListener('click', function () {
+    topLine.classList.toggle("burger__topline--open");
+    midLine.classList.toggle("burger__midline--open");
+    btmLine.classList.toggle("burger__btmline--open");
+  });
+}
+
+;
 //# sourceMappingURL=all.js.map
