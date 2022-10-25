95626b6e499df9b8c55784d4a0bcd4a42b8534e4
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-10-25 23:24:02 +0800

    新增(layout.js)：完成漢堡選單的 toggle class

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,17 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
 });
+
+// burger
+const burger = document.querySelector('.burger');
+const topLine = document.querySelector('.burger__topline');
+const midLine = document.querySelector('.burger__midline');
+const btmLine = document.querySelector('.burger__btmline');
+
+if (burger) {
+  burger.addEventListener('click', () => {
+    topLine.classList.toggle("burger__topline--open");
+    midLine.classList.toggle("burger__midline--open");
+    btmLine.classList.toggle("burger__btmline--open");
+  });
+};
