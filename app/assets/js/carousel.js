ecc121fafb0758950e858af891ded207a88ca22e
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-10-27 19:21:57 +0800

    完成(index.html)：'不同類別的風格模板'區塊完成

---
+++ app/assets/js/carousel.js
@@ -0,0 +1,23 @@
+var resumeSwiper = new Swiper(".resumeSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 12,
+  grabCursor: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+    992: {
+      slidesPerView: 3
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
