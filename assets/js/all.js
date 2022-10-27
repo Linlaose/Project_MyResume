e331cb1842d09e3b88fc9b91fe5a64743042c5af
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-10-27 19:22:22 +0800

    Update 2022-10-27T11:22:16.718Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -18,4 +18,29 @@ if (burger) {
 }
 
 ;
+"use strict";
+
+var resumeSwiper = new Swiper(".resumeSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 12,
+  grabCursor: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48
+    },
+    992: {
+      slidesPerView: 3
+    }
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
 //# sourceMappingURL=all.js.map
