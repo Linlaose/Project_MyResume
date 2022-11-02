7ed7dde0b33dd863b2b6532fdab97f45d8be3aff
Ryan Tsai <a0986575797@gmail.com>
2022-11-03 01:27:09 +0800

    新增(admin-*.html)：管理履歷類別頁面完成

--- app/assets/js/carousel.js
+++ app/assets/js/carousel.js
@@ -21,3 +21,65 @@ var resumeSwiper = new Swiper(".resumeSwiper", {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+// 管理頁面履歷類別頁
+var headerSwiper = new Swiper(".headerSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 12,
+  grabCursor: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var contentSwiper = new Swiper(".contentSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 12,
+  grabCursor: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var footerSwiper = new Swiper(".footerSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 12,
+  grabCursor: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
