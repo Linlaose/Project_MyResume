4049cc07893f73726a86b7dd3e410d2303976264
Ryan Tsai <a0986575797@gmail.com>
2022-11-03 01:27:34 +0800

    Update 2022-11-02T17:27:28.282Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -42,5 +42,63 @@ var resumeSwiper = new Swiper(".resumeSwiper", {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
   }
+}); // 管理頁面履歷類別頁
+
+var headerSwiper = new Swiper(".headerSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 12,
+  grabCursor: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2
+    }
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+var contentSwiper = new Swiper(".contentSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 12,
+  grabCursor: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2
+    }
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+var footerSwiper = new Swiper(".footerSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 12,
+  grabCursor: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2
+    }
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 });
 //# sourceMappingURL=all.js.map
