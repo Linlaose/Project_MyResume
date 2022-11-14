9ee716cddecc2560ce82c760447b9ef1385336c3
Ryan Tsai <a0986575797@gmail.com>
2022-11-14 10:05:06 +0800

    更新(member.js)：會員功能完成

--- app/assets/js/member.js
+++ app/assets/js/member.js
@@ -1,26 +1,36 @@
 function signUp() {
   const apiUrl = "http://localhost:3000/register";
   const obj = {
-    "email": "olivier@mail.com",
-    "password": "bestPassw0rd"
+    "email": "a123@mail.com",
+    "password": "1qaz2wsx",
+    "name": "Ryan"
   };
   axios.post(apiUrl, obj)
     .then((res) => {
-      console.log(res.data);
-    })
-}
-function login() {
-  const apiUrl = "http://localhost:3000/login";
-  const data = {
-    "email": "olivier@mail.com",
-    "password": "bestPassw0rd"
-  };
-  axios.post(apiUrl, data)
-    .then((res) => {
-      console.log(res.data, `狀態碼 ${res.status}`);
+      console.log(res);
     }).catch((err) => {
       console.log(err);
     })
-}
+};
+function login() {
+  const apiUrl = "http://localhost:3000/login";
+  const data = {
+    "email": "a123@mail.com",
+    "password": "1qaz2wsx"
+  };
+
+  axios.post(apiUrl, data)
+    .then((res) => {
+      localStorage.setItem("email", JSON.stringify(res.data.user.email));
+      localStorage.setItem("userName", JSON.stringify(res.data.user.name));
+      localStorage.setItem("token", JSON.stringify(res.data.accessToken));
+    }).catch((err) => {
+      console.log(err.response.data);
+    })
+};
+function logout() {
+  window.localStorage.clear();
+};
 // signUp();
-login();
+// login();
+logout();
